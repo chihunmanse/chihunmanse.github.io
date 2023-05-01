6d2d90a8db53e8d00086a0be0c72004d0e07c6ad
chihunmanse <chihunmanse@gmail.com>
2023-05-01 21:53:44 +0900

    Deploy website - based on 545d24faf51d6904ce398b29badca85d49d52995

---
+++ assets/js/2ce83258.f8fb0ad6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[381],{8202:e=>{e.exports=JSON.parse('{"title":"Upgradeable Contract","slug":"/category/upgradeable-contract","permalink":"/docs/category/upgradeable-contract","navigation":{"next":{"title":"Manage Docs Versions","permalink":"/docs/blockchain/upgradeable_contract/contract"}}}')}}]);
