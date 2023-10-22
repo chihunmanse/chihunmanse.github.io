0737f82297b3ccf6592843f8bd4bad0be3076aaa
chihunmanse <chihunmanse@gmail.com>
2023-10-22 21:40:56 +0900

    add : docker 개념 정리

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -135,7 +135,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ["solidity"],
+        additionalLanguages: ["solidity", 'docker'],
       },
     }),
 };
